b20e7340a58d8dfc85b8398c7d40c7e29750e0c6
confident-token-1019 <theoonetj@gmail.com>
2020-01-25 19:59:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
 # Add your code here
+
+class Dog
+ 
+  @@all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def .print_all
+    @@Dog.each{|dog| puts dog}
+  
+end
+  end
+
+
+
+
+ Dog.new("Pluto") 
+
